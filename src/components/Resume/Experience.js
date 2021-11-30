83423fb84091175c8dff87b36e5216b34ce8053b
healthindiawellbeing <healthindiawellbeing@gmail.com>
2021-11-30 13:53:30 +0530

    Commit diet chart

--- src/components/Resume/Experience.js
+++ src/components/Resume/Experience.js
@@ -4,11 +4,7 @@ const { PUBLIC_URL } = process.env;
 
 const Experience = () => (
   <div className="experience">
-    <img src={`${PUBLIC_URL}/images/brokenwheat.jpeg`} alt="" width="238" height="227" />
-    <img src={`${PUBLIC_URL}/images/brownrice.jpeg`} alt="" width="238" height="227" />
-    <img src={`${PUBLIC_URL}/images/ragi.jpeg`} alt="" width="238" height="227" />
-    <img src={`${PUBLIC_URL}/images/fenugreek.jpeg`} alt="" width="238" height="227" />
-    <img src={`${PUBLIC_URL}/images/sorghum.jpeg`} alt="" width="238" height="227" />
+    <img src={`${PUBLIC_URL}/images/ingredients.jpeg`} alt="" width="100%" height="100%" object-fit="cover" />
     <br />
     <br />
     <div className="link-to" id="experience" />
